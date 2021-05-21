8a13636ab5b1683f97bb56056671b408aca70243
Thom Gatsjadoerian <thom@sodaq.com>
2021-05-21 10:49:38 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,12 @@ void setup()
     // Some questions:
     // 1. What is the protocol used to communicate with the TMP1075 sensor?
     // 2. The code uses two specific values, 0x48 and 0x0F, what are they and what are they used for?
-    // 3. The result is stored, with this result something can be confirmed. What's the purpose and with what value should this be compared to?
-    // 4. This code uses so called "magic numbers", this is typically considered practice. How would you improve this?
-    // 5. Can you explain in a couple sentences what this code does?
+    // 3. We retrieve 2 bytes from the sensor on line 16, using the datasheet, why do we need to shift the first read 8 to the left and OR it with the second read?
+    // 4. The datasheet mentions that the communication is MSB first, what would have to change here if it was LSB first instead?
+    // 5. The result of the read is stored, with this value something can be confirmed. What's the purpose of doing this and with what value should this be compared to?
+    // 6. This code uses so called "magic numbers", this is typically considered bad practice. How would you improve this?
+    // 7. Can you explain in a couple sentences what this code does?
+    // 8. Extra: On line 16 we don't need to perform 2 read instructions. Why not? What would the simplified code (and comparison) look like?
 }
 
 void loop() {}
